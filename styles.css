c10e1e90e6fba59487cb5565c3fb87e59f5b5edb
SHANLIGE <69331343+SHANLIGE@users.noreply.github.com>
2020-08-22 17:55:05 -0500

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,34 @@
+#html{
+    margin: 0;
+    padding: 0;
+
+}
+#body{
+    
+    align-items: center;
+    width: 100%;
+ 
+    }
+    
+    
+
+
+
+#text{
+   
+    font-size: 20px;
+    margin: auto;
+    margin-top: 200px;
+    margin-bottom: 200px;
+    width: 50%;
+    padding: 30px;
+    text-align: center;
+    color: rgb(189, 127, 247);
+    font-family: sans-serif;
+    border: blueviolet;
+    border-style: dotted;
+    border-radius: 5rem;
+    
+     }
+   
+    
